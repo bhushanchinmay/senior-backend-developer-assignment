d8e7e095157708e9895541c4c363e14863e3857f
Chinmay Bhushan <bhushan.chinmay@gmail.com>
2021-01-03 23:55:02 +0530

    initial commit

---
+++ rbac/rbacapp/models.py
@@ -0,0 +1,65 @@
+
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+
+# Register own model admin, based on the default UserAdmin
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+
+    # Not editable in the admin default change form
+    readonly_fields = [
+        'date_joined',
+    ]
+
+    # Admin action to mark multiple users as active
+    actions = [
+        'activate_users',
+    ]
+
+    def activate_users(self, request, queryset):
+        cnt = queryset.filter(is_active=False).update(is_active=True)
+        self.message_user(request, 'Activated {} users.'.format(cnt))
+    activate_users.short_description = 'Activate Users'  # type: ignore
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if not request.user.has_perm('auth.change_user'):
+            del actions['activate_users']
+        return actions
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+        disabled_fields = set()
+
+        # Prevent non-superusers from changing a user’s username and other resources
+        if not is_superuser:
+            disabled_fields |= {
+                'username',
+                'is_superuser',
+                'user_permissions',
+            }
+
+        # Prevent non-superusers from editing their own permissions
+        if (
+            not is_superuser
+            and obj is not None
+            and obj == request.user
+        ):
+            disabled_fields |= {
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions',
+            }
+
+        for f in disabled_fields:
+            if f in form.base_fields:
+                form.base_fields[f].disabled = True
+
+        return form
+    
+    # To prevent staff users from deleting a model instance, regardless of their permissions
+    def has_delete_permission(self, request, obj=None):
+        return False
