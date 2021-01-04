ae47fc66b370d39ff2726b462eac207c33ee6535
Chinmay Bhushan <bhushan.chinmay@gmail.com>
2021-01-04 10:20:47 +0530

    removed secret key from settings.py

--- rbac/rbac/settings.py
+++ rbac/rbac/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vz*r&!0@u-tx3dml9knc_s98u%)t92dqrko=1%v5*==of%w+0#'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
